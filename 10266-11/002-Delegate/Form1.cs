a5b28069848f309d4c667aa2b6b2bbebceaf9f19
Agnaldo <agnaldo@50minutos.com.br>
2013-01-24 16:26:46 -0200

    Revert "add files"
    This reverts commit 9dc26ded885155795bb3b77a5057bc6ae03d22b4.

--- 10266-11/002-Delegate/Form1.cs
+++ 10266-11/002-Delegate/Form1.cs
@@ -11,9 +11,6 @@ namespace _002_Delegate
 {
     public partial class Form1 : Form
     {
-        double numero;
-        String operacao;
-
         public Form1()
         {
             InitializeComponent();
@@ -100,49 +97,6 @@ namespace _002_Delegate
             n = n * Math.PI / 180;
 
             n = Math.Tan(n);
-            
-            x.Text = n.ToString();
-        }
-
-        private void button22_Click(object sender, EventArgs e)
-        {
-            //abs
-            double n = Math.Abs(Double.Parse(x.Text));
-
-            x.Text = n.ToString();
-        }
-
-        private void button21_Click(object sender, EventArgs e)
-        {
-            //sign
-            double n = Math.Sign(Double.Parse(x.Text));
-
-            x.Text = n.ToString();
-        }
-
-        private void Operacao(object sender, EventArgs e)
-        {
-            //+-*/%
-            numero = Double.Parse(x.Text);
-            operacao = ((Button)sender).Text;
-
-            x.Focus();
-            x.SelectAll();
-        }
-
-        private void button29_Click(object sender, EventArgs e)
-        {
-            //=
-            double n = Double.Parse(x.Text);
-
-            switch (operacao)
-            {
-                case "+": n = numero + n; break;
-                case "-": n = numero - n; break;
-                case "*": n = numero * n; break;
-                case "/": n = numero / n; break;
-                case "%": n = numero % n; break;
-            }
 
             x.Text = n.ToString();
         }
